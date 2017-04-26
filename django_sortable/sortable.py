e10424dd4e6f50a773e27b3f83d211fc54bc2e5a
Tõnis Väin <tonis@namespace.ee>
2017-04-26 16:13:52 +0300

    reformat

--- django_sortable/sortable.py
+++ django_sortable/sortable.py
@@ -12,131 +12,127 @@ class SortableInvalidObjectsException(Exception): pass
 
 
 class Sortable(object):
-  """docstring for Sortable"""
+    """docstring for Sortable"""
 
-  def __init__(self, objects, fields):
-    super(Sortable, self).__init__()
-    self.objects = objects
-    self.fields = None
-    self.set_normalized_fields(fields)
+    def __init__(self, objects, fields):
+        super(Sortable, self).__init__()
+        self.objects = objects
+        self.fields = None
+        self.set_normalized_fields(fields)
 
+    def set_normalized_fields(self, fields):
+        """Takes name-to-field mapping tuple, normalizes it, and sets field."""
+        if fields is None:
+            return
 
-  def set_normalized_fields(self, fields):
-    """Takes name-to-field mapping tuple, normalizes it, and sets field."""
-    if fields is None:
-      return
+        field_list = []
+        for f in fields:
+            if isinstance(f, six.string_types):
+                field_list.append((f, (f,)))
+            elif isinstance(f[1], six.string_types):
+                field_list.append((f[0], (f[1],)))
+            else:
+                field_list.append(f)
+        self.fields = dict(field_list)
 
-    field_list = []
-    for f in fields:
-      if isinstance(f, six.string_types):
-        field_list.append((f, (f,)))
-      elif isinstance(f[1], six.string_types):
-        field_list.append((f[0], (f[1],)))
-      else:
-        field_list.append(f)
-    self.fields = dict(field_list)
+    def sorted(self, field_name, direction='asc'):
+        """Returns QuerySet with order_by applied or sorted list of dictionary."""
 
+        if self.fields:
+            try:
+                fields = self.fields[field_name]
+            except KeyError:
+                return self.objects
+        else:
+            fields = (field_name,)
 
-  def sorted(self, field_name, direction='asc'):
-    """Returns QuerySet with order_by applied or sorted list of dictionary."""
+        if direction not in ('asc', 'desc'):
+            return self.objects
 
-    if self.fields:
-      try:
-        fields = self.fields[field_name]
-      except KeyError:
-        return self.objects
-    else:
-      fields = (field_name,)
+        fields = Sortable.prepare_fields(fields, direction)
 
-    if direction not in ('asc', 'desc'):
-      return self.objects
+        if hasattr(self.objects, 'order_by'):
+            result = self.objects.order_by(*fields)
+        elif isinstance(self.objects, (list, tuple)):
+            if len(self.objects) < 2:
+                return self.objects
 
-    fields = Sortable.prepare_fields(fields, direction)
+            comparers = []
+            getter = itemgetter if isinstance(self.objects[0], dict) else attrgetter
+            for f in fields:
+                field = f[1:] if f.startswith('-') else f
+                comparers.append((getter(field), 1 if field == f else -1))
 
-    if hasattr(self.objects, 'order_by'):
-      result = self.objects.order_by(*fields)
-    elif isinstance(self.objects, (list, tuple)):
-      if len(self.objects) < 2:
-        return self.objects
+            def comparer(left, right):
+                for func, polarity in comparers:
+                    result = cmp(func(left), func(right))
+                    return 0 if not result else polarity * result
 
-      comparers = []
-      getter = itemgetter if isinstance(self.objects[0], dict) else attrgetter
-      for f in fields:
-        field = f[1:] if f.startswith('-') else f
-        comparers.append((getter(field), 1 if field == f else -1))
+            if six.PY2:
+                result = sorted(self.objects, cmp=comparer)
+            else:
+                result = sorted(self.objects, key=functools.cmp_to_key(comparer))
+        else:
+            raise SortableInvalidObjectsException('An object of this type can not be sorted.')
 
-      def comparer(left, right):
-        for func, polarity in comparers:
-          result = cmp(func(left), func(right))
-          return 0 if not result else polarity * result
+        return result
 
-      if six.PY2:
-        result = sorted(self.objects, cmp=comparer)
-      else:
-        result = sorted(self.objects, key=functools.cmp_to_key(comparer))
-    else:
-      raise SortableInvalidObjectsException('An object of this type can not be sorted.')
+    def sql_predicate(self, field_name, direction='asc', default=None):
+        """Returns a predicate for use in a SQL ORDER BY clause."""
 
-    return result
+        if self.fields:
+            try:
+                fields = self.fields[field_name]
+            except KeyError:
+                fields = default
+        else:
+            fields = field_name
 
+        if direction not in ('asc', 'desc'):
+            fields = default
 
-  def sql_predicate(self, field_name, direction='asc', default=None):
-    """Returns a predicate for use in a SQL ORDER BY clause."""
+        fields = Sortable.prepare_fields(fields, direction, sql_predicate=True)
+        return ', '.join(fields)
 
-    if self.fields:
-      try:
-        fields = self.fields[field_name]
-      except KeyError:
-        fields = default
-    else:
-      fields = field_name
+    @staticmethod
+    def prepare_fields(fields, direction, sql_predicate=False):
+        """Given a list or tuple of fields and direction, return a list of fields 
+        with their appropriate order_by direction applied.
+    
+        >>> fields = ['++one', '--two', '+three', 'four', '-five']
+        >>> Sortable.prepare_fields(fields, 'asc')
+        ['one', '-two', 'three', 'four', '-five']
+        >>> Sortable.prepare_fields(fields, 'desc')
+        ['one', '-two', '-three', '-four', 'five']
+        >>> Sortable.prepare_fields(fields, 'not_asc_or_desc')
+        ['one', '-two', 'three', 'four', '-five']
+        >>> Sortable.prepare_fields(fields, 'desc', True)
+        ['one ASC', 'two DESC', 'three DESC', 'four DESC', 'five ASC']
+        """
 
-    if direction not in ('asc', 'desc'):
-      fields = default
+        if direction not in ('asc', 'desc'):
+            direction = 'asc'
 
-    fields = Sortable.prepare_fields(fields, direction, sql_predicate=True)
-    return ', '.join(fields)
+        fields = list(fields)
+        for i, field in enumerate(fields):
+            if field.startswith('--'):
+                fields[i] = field[1:]
+            elif field.startswith('++'):
+                fields[i] = field[2:]
+            elif field.startswith('-'):
+                fields[i] = (direction == 'asc' and '-' or '') + field[1:]
+            else:
+                field = field[1:] if field.startswith('+') else field
+                fields[i] = (direction == 'desc' and '-' or '') + field
 
+        if not sql_predicate:
+            return fields
 
-  @staticmethod
-  def prepare_fields(fields, direction, sql_predicate=False):
-    """Given a list or tuple of fields and direction, return a list of fields 
-    with their appropriate order_by direction applied.
-
-    >>> fields = ['++one', '--two', '+three', 'four', '-five']
-    >>> Sortable.prepare_fields(fields, 'asc')
-    ['one', '-two', 'three', 'four', '-five']
-    >>> Sortable.prepare_fields(fields, 'desc')
-    ['one', '-two', '-three', '-four', 'five']
-    >>> Sortable.prepare_fields(fields, 'not_asc_or_desc')
-    ['one', '-two', 'three', 'four', '-five']
-    >>> Sortable.prepare_fields(fields, 'desc', True)
-    ['one ASC', 'two DESC', 'three DESC', 'four DESC', 'five ASC']
-    """
-
-    if direction not in ('asc', 'desc'):
-      direction = 'asc'
-
-    fields = list(fields)
-    for i, field in enumerate(fields):
-      if field.startswith('--'):
-        fields[i] = field[1:]
-      elif field.startswith('++'):
-        fields[i] = field[2:]
-      elif field.startswith('-'):
-        fields[i] = (direction == 'asc' and '-' or '') + field[1:]
-      else:
-        field = field[1:] if field.startswith('+') else field
-        fields[i] = (direction == 'desc' and '-' or '') + field
-
-    if not sql_predicate:
-      return fields
-
-    # determine sql predicates from list
-    fields = list(fields)
-    for i, field in enumerate(fields):
-      if field.startswith('-'):
-        fields[i] = '%s DESC' % (field[1:],)
-      else:
-        fields[i] = '%s ASC' % (field,)
-    return fields
+        # determine sql predicates from list
+        fields = list(fields)
+        for i, field in enumerate(fields):
+            if field.startswith('-'):
+                fields[i] = '%s DESC' % (field[1:],)
+            else:
+                fields[i] = '%s ASC' % (field,)
+        return fields
